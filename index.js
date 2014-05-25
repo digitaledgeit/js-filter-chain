ee1af0fa7810fce51f5e0f0663353c7afdaa6d6a
James Newell <james@digitaledgeit.com.au>
2014-05-25 23:48:28 +1000

    Updated to work with callbacks

--- index.js
+++ index.js
@@ -1,38 +1,71 @@
 /**
- * A collection of filters
+ * A filter chain
  * @constructor
+ * @params  {object[]}      filters
  */
-function FilterCollection(filters) {
-  this.filters = [];
-  if (filters) {
-    for (var i=0; i<filters.length; ++i) {
-      this.add(filters[i]);
-    }
-  }
+function FilterChain(filters) {
+
+	//create a new instance of the chain
+	if (!(this instanceof FilterChain)) {
+		return new FilterChain(filters);
+	}
+
+	//initialise the chain with the provided filters
+	this.filters = [];
+	if (filters) {
+		for (var i = 0; i < filters.length; ++i) {
+			this.add(filters[i]);
+		}
+	}
+
 }
 
 /**
- * Adds a filter to the collection
- * @param   {function}  filter
- * @returns {FilterCollection}
+ * Adds a filter
+ * @param   {function}      filter
+ * @returns {FilterChain}
  */
-FilterCollection.prototype.add = function(filter) {
-  this.filters.push(filter);
-  return this;
+FilterChain.prototype.add = function (filter) {
+	this.filters.push(filter);
+	return this;
 }
 
 /**
- * Run all the filters on the value
- * @param   {*} value
- * @returns {*}
+ * Filters a value
+ * @param   {*}             value       The value to filter
+ * @param   {function}      callback    The callback to call when filtering is complete
+ * @returns {FilterChain}
  */
-FilterCollection.prototype.filter = function(value) {
-  for (var i=0; i<this.filters.length; ++i) {
-    value = this.filters[i](value);
-  }
-  return value;
+FilterChain.prototype.filter = function (value, callback) {
+	var self = this, count = 0;
+
+	/**
+	 * Checks whether the last callback has been called
+	 * @param   {*} val
+	 */
+	function finish(val) {
+		value = val;
+		if (++count >= self.filters.length) {
+			callback(value);
+		}
+	}
+
+	//run each filter on the value
+	for (var i = 0; i < this.filters.length; ++i) {
+
+		var filter = this.filters[i];
+
+		if (filter.length > 1) {
+			filter(value, finish); //async
+		} else {
+			finish(filter(value)); //sync
+		}
+
+	}
+
+	return this;
 }
 
-module.exports = FilterCollection;
+module.exports = FilterChain;
 
 
